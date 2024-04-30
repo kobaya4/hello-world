816d14f19459f9a5950f2af82e75f64e9ecf65f2
kobayashi <kobaya4@gmail.com>
2024-04-30 14:11:29 +0900

    ボタン追加

--- NHKGendaiAnkiBookmarklet.js
+++ NHKGendaiAnkiBookmarklet.js
@@ -16,24 +16,8 @@ export default () => {
     let jTitle = document.getElementsByClassName('title')[0].innerText;
     let eTitle = document.getElementsByClassName('gc-article-text')[0].getElementsByTagName("div")[0].getElementsByTagName("p")[0].getElementsByTagName("strong")[0].innerText;
     let titeLine = `${titleDate} <a href="${url}"><br>${jTitle}</a><br>${eTitle}`;
-    /*
-    2024-04-22 (月) <a href="https://www.nhk.jp/p/rs/77RQWQX1L6/episode/re/P8M3MQ16X4"><br>大使館突入でメキシコがエクアドルと断交</a><br>MEXICO CUTS DIPLOMATIC TIES WITH ECUADOR OVER EMBASSY RAID
-        var d = new Date(year + '-' + month + '-' + day);
-        var dayList =['日','月','火','水','木','金','土'];
-        var titleLine = year + '-' + month + '-' + day + ' (' + dayList[d.getDay()] + ') ';
-    */
-        //let releaseDate = document.getElementsByClassName('release-date')[0].innerText;
-
-
-    //let body = document.getElementsByClassName('sc-jtggT');
-    //let chapter = document.getElementsByClassName('sc-gmeYpB');
-
-    /* 抜き出したニュース記事をテキスト形式に整形する */
-    //let text = `${jTitle}\n`;
-    //text += `${eTitle}\n\n`;
-    //text += `${date_time}\n\n`;
+ 
     let sentences = document.querySelectorAll("#article-1 > div.gc-article-text > div > p");
-    //text += `${sentences.length}\n`;
     let text = "";
     for (let i = 0; i < sentences.length / 7; i++) {
         let eLine = sentences[i * 7].querySelector("strong").innerText;
@@ -42,10 +26,17 @@ export default () => {
         text += `<details><summary>${jLine}<br></summary><br><div>${eLine}</div>[sound:NHKGendai${onAirDate}_${sentenseNo}.aac]</details><hr>`;
     }
     let insert = document.querySelector("#app > div.main-content-wrapper > div:nth-child(1) > section > div.tabs-wrapper > div.tabs-contents > section:nth-child(4) > section > div.episode-detail-article-contents");
+
+    let btn = document.createElement("button");
+    btn.innerHTML = "Ankiへ追加";
+    btn.setAttribute("id","AddCard2Anki");
+    insert.appendChild(btn);
+
     let titleLineDiv = document.createElement("div");
     titleLineDiv.className = 'titleline';
     titleLineDiv.innerHTML = titeLine;
     insert.appendChild(titleLineDiv);
+    
     let contentDiv = document.createElement("div");
     contentDiv.className = 'content';
     contentDiv.innerHTML = text;
